d175dd74cb1878c825f7f347b63fe3cd80b7b7f3
HW-BC <zhangxuhe96@163.com>
2023-07-07 07:04:01 +0800

    Personaldiary

---
+++ app/src/main/java/com/example/diaryL/data/LoginDataSource.java
@@ -0,0 +1,28 @@
+package com.example.diaryL.data;
+
+import com.example.diaryL.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * 处理带有w/登录凭据的身份验证并检索用户信息的类。
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
